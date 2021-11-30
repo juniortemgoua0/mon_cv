1973f7278fdd736a470d5183b2981b3fe8ff85b5
juniortemgoua0 <juniortemgoua0@gmail.com>
2021-11-30 23:55:06 +0100

    test2

--- components/Profil.component.php
+++ components/Profil.component.php
@@ -51,7 +51,6 @@
         <div class="user_name">
             <h2>TEMGOUA NGOUSSONG <br> Browdon Junior</h2>
             <p class="user_job">Architecte logiciel / Devops</p>
-            <p> bonjour </p>
         </div>
 
     </div>
